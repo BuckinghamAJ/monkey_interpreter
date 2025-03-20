adaf18b8779b404e46d458ea0f2bd5ca441304f6
Adam Buckingham <48027257+BuckinghamAJ@users.noreply.github.com>
2025-03-20 06:34:55 -0400

    Int Literals displayed in REPL

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey_interpreter/evaluator"
 	"monkey_interpreter/lexer"
 	"monkey_interpreter/parser"
 )
@@ -29,17 +30,18 @@ func Start(in io.Reader, out io.Writer) {
 
 		if len(p.Errors()) != 0 {
 			printParseErrors(out, p.Errors())
-			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
 
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
-
 }
 
-func printParseErrors(out io.Writer, errors []string){
+func printParseErrors(out io.Writer, errors []string) {
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
