4d17c475dbe7f323c2d74346065ad7a95498420d
Adam Buckingham <48027257+BuckinghamAJ@users.noreply.github.com>
2025-04-11 07:02:44 -0400

    Builtin-Functions added

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -91,3 +92,12 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type BuiltinFunctions func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunctions
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
